159e039e59e8fc99e041675b81a00e307a01454b
iepping1 <Medigoultima@gmail.com>
2018-12-03 13:59:45 +0100

    Near completed project

--- app/src/main/java/epping/ian/tictactoe/Game.java
+++ app/src/main/java/epping/ian/tictactoe/Game.java
@@ -1,29 +1,37 @@
 package epping.ian.tictactoe;
 
-public class Game {
+import java.io.Serializable;
 
-    // sets the size of the board
+public class Game implements Serializable{
+
+    // set up board
     final private int BOARD_SIZE = 3;
     private Tile[][] board;
 
-    // setting up the two players
+    // set up two players
     private Boolean playerOneTurn;
+
+    // set up gameover state
     private Boolean gameOver;
+
+    // track number of moves
+    private int moves = 0;
 
     public Game() {
 
-        // fills the board with blank tiles
+        // fill board with blank tiles
         board = new Tile[BOARD_SIZE][BOARD_SIZE];
         for (int i=0; i<BOARD_SIZE; i++)
             for (int j=0; j<BOARD_SIZE; j++)
                 board[i][j] = Tile.BLANK;
 
-        // player one always starts the game
+        // player one always starts game
         playerOneTurn = true;
         gameOver = false;
+        moves = 0;
     }
 
-    // draws the crosses and circles
+    // draw cross or circle in tile
     public Tile draw(int row, int column) {
 
         if(board[row][column]==Tile.BLANK){
@@ -32,6 +40,7 @@ public class Game {
             if(playerOneTurn){
                 board[row][column] = Tile.CROSS;
                 playerOneTurn = false;
+                moves += 1;
                 return Tile.CROSS;
             }
 
@@ -39,11 +48,65 @@ public class Game {
             else{
                 board[row][column] = Tile.CIRCLE;
                 playerOneTurn = true;
+                moves += 1;
                 return Tile.CIRCLE;
             }
         }
         // illegal moves
         else
             return Tile.INVALID;
+    }
+
+    // check for endgame
+    public GameState ended() {
+
+        // check for stalemate
+        if (moves == (BOARD_SIZE * BOARD_SIZE)) {
+            return GameState.DRAW;
+        }
+
+        for (int i = 0; i < BOARD_SIZE; i++) {
+
+            // check for horizontal victory
+            if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+                if (board[i][0] == Tile.CROSS) {
+                    return GameState.PLAYER_ONE;
+                }
+                if (board[i][0] == Tile.CIRCLE) {
+                    return GameState.PLAYER_TWO;
+                }
+            }
+            // check for vertical victory
+            if (board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+                if (board[0][i] == Tile.CROSS) {
+                    return GameState.PLAYER_ONE;
+                }
+                if (board[0][i] == Tile.CIRCLE) {
+                    return GameState.PLAYER_TWO;
+                }
+            }
+        }
+
+        // check for diagonal victory
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            if (board[0][0] == Tile.CROSS) {
+                return GameState.PLAYER_ONE;
+            }
+            if (board[0][0] == Tile.CIRCLE) {
+                return GameState.PLAYER_TWO;
+            }
+        }
+
+        else if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            if (board[0][2] == Tile.CROSS) {
+                return GameState.PLAYER_ONE;
+            }
+            if (board[0][2] == Tile.CIRCLE) {
+                return GameState.PLAYER_TWO;
+            }
+        }
+
+        // game stil ongoing
+        return GameState.IN_PROGRESS;
     }
 }
--- app/src/main/java/epping/ian/tictactoe/MainActivity.java
+++ app/src/main/java/epping/ian/tictactoe/MainActivity.java
@@ -4,51 +4,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // declares the variables
+    // declare board variables
     Game game;
     int row;
     int column;
 
-    // creates an array for all the button IDs
+    // array for all button IDs
     public int[] buttons = new int[]{R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5,
             R.id.button6, R.id.button7, R.id.button8, R.id.button9};
 
-    // creates an array for the button names
-    public String[] names = {"Button1", "Button2", "Button3", "Button4", "Button5", "Button6",
+    // array for button names
+    public String[] button_names = {"Button1", "Button2", "Button3", "Button4", "Button5", "Button6",
             "Button7", "Button8", "Button9"};
 
-    // variable for the total number of images
-    public int length = names.length;
+    // total number of images
+    public int length = button_names.length;
 
+    // start game
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         game = new Game();
         setContentView(R.layout.activity_main);
 
-        // restores the main window after reload
+        //restores the main window after reload or orientation switch
         if(savedInstanceState != null) {
             for (int i = 0; i < length; i++) {
                 Button button = findViewById(buttons[i]);
-                button.setText(savedInstanceState.getString("Button"));
+                if(button != null) {
+                    button.setText(savedInstanceState.getString( button_names[i]));
+                }
             }
         }
     }
 
-    // saves the board settings after a window reload
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState); //always call super
-
-        for (int i = 0; i < length; i++) {
-            Button button = findViewById(buttons[i]);
-            outState.putString("Button", (String) button.getText());
-        }
-    }
-
-    // assigns the buttons their coordinates
+    // assign button coordinates
     public void tileClicked(View view) {
         int id = view.getId();
         if(id==R.id.button1 || id == R.id.button2 || id == R.id.button3){
@@ -75,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             column = 2;
         }
 
-        // sets up the possible game states
+        // possible game states
         Tile tile = game.draw(row, column);
         Button button= findViewById(id);
 
@@ -89,15 +83,47 @@ public class MainActivity extends AppCompatActivity {
             case INVALID:
                 break;
         }
+
+        // check for game end
+        GameState gameState = game.ended();
+
+        switch(gameState) {
+
+            case PLAYER_ONE:
+                TextView OneWon = findViewById(R.id.message);
+                OneWon.setText("Player 1 Won!");
+                break;
+            case PLAYER_TWO:
+                TextView TwoWon = findViewById(R.id.message);
+                TwoWon.setText("Player 2 Won!");
+                break;
+            case DRAW:
+                TextView draw = findViewById(R.id.message);
+                draw.setText("It's a Tie!");
+                break;
+        }
     }
 
-    // clears the board when clicked
+    // clear board when clicked
     public void resetClicked(View view){
         game = new Game();
 
         for (int i = 0; i < length; i++) {
             Button button = findViewById(buttons[i]);
             button.setText("");
+        }
+
+        TextView message = findViewById(R.id.message);
+        message.setText("");
+    }
+
+    // save board settings after window reload/reorientation
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        for (int i = 0; i < length; i++) {
+            Button button = findViewById(buttons[i]);
+            outState.putString(button_names[i], (String) button.getText());
         }
     }
 }
